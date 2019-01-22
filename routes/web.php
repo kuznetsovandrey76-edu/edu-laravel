dd78566d1972010dfbdd2ff047f2a35f3cd13f46
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-01-22 19:07:02 +0300

    work with Model

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
-
+Route::get('/task', 'TasksController@index');
 
